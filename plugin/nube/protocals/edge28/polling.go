68abc6a655124167d8dbc8de75a5f91a508eae5d
Marc Dallen <marc@enviroservices.com.au>
2021-11-01 18:35:52 +1100

    WIP.

--- plugin/nube/protocals/edge28/polling.go
+++ plugin/nube/protocals/edge28/polling.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/NubeDev/flow-framework/api"
 	"github.com/NubeDev/flow-framework/model"
 	edgerest "github.com/NubeDev/flow-framework/plugin/nube/protocals/edge28/restclient"
 	"github.com/NubeDev/flow-framework/src/poller"
 	"github.com/NubeDev/flow-framework/utils"
+	"github.com/NubeIO/nubeio-rubix-lib-helpers-go/pkg/edge28"
+	"github.com/NubeIO/nubeio-rubix-lib-helpers-go/pkg/numbers"
+	"github.com/NubeIO/nubeio-rubix-lib-helpers-go/pkg/thermistor"
 	log "github.com/sirupsen/logrus"
+	"reflect"
 	"time"
 )
 
@@ -44,16 +50,6 @@ func (i *Instance) processWrite(pnt *model.Point, value float64, rest *edgerest.
 	}
 }
 
-//GPIOValueToDigital
-//TODO remove this and get from helpers
-func GPIOValueToDigital(value float64) float64 {
-	if value < 0.2 {
-		return 1 //ON / Closed Circuit
-	} else { //previous functions used > 0.6 as an OFF threshold.
-		return 0 //OFF / Open Circuit
-	}
-}
-
 func (i *Instance) processRead(pnt *model.Point, value float64, pollCount float64) (float64, error) {
 	cov := utils.Float64IsNil(pnt.COV) //TODO add in point scaling to get COV to work correct (as in scale temp or 0-10)
 	covEvent, _ := utils.COV(value, utils.Float64IsNil(pnt.PresentValue), cov)
@@ -81,7 +77,6 @@ func (i *Instance) processRead(pnt *model.Point, value float64, pollCount float6
 
 	}
 	return value, nil
-
 }
 
 func (i *Instance) polling(p polling) error {
@@ -112,10 +107,12 @@ func (i *Instance) polling(p polling) error {
 			time.Sleep(15000 * time.Millisecond)
 			log.Info("edge-28: NO NETWORKS FOUND")
 		}
+
 		for _, net := range nets { //NETWORKS
 			if net.UUID != "" && net.PluginConfId == i.pluginUUID {
 				log.Infof("edge-28: LOOP COUNT: %v\n", counter)
 				counter++
+
 				for _, dev := range net.Devices { //DEVICES
 					if err != nil {
 						log.Errorf("edge-28: failed to vaildate device %v %s\n", err, dev.CommonIP.Host)
@@ -125,39 +122,75 @@ func (i *Instance) polling(p polling) error {
 					getDI, err = rest.GetDIs()
 					dNet := p.delayNetworks
 					time.Sleep(dNet)
+
 					for _, pnt := range dev.Points { //POINTS
-						var _pnt model.Point
-						var pv float64
-						if pnt.Priority != nil { //WRITE TODO add in all the extra GPIO
-							var wv float64
-							if (*pnt.Priority).P16 != nil {
-								wv = *pnt.Priority.P16
-							}
-							switch pnt.IoID {
-							case pointList.R1, pointList.R2:
-								if wv >= 1 {
-									wv = 1
-								} else {
-									wv = 0
+						var rv float64
+						var readValStruct interface{}
+						var readValType string
+						var wv float64
+						fmt.Println(pnt.IoID)
+						switch pnt.IoID {
+						//OUTPUTS
+						case pointList.R1, pointList.R2, pointList.DO1, pointList.DO2, pointList.DO3, pointList.DO4, pointList.DO5:
+							if pnt.Priority != nil {
+								wv, err = edge28.DigitalToGPIOValue(wv)
+								if err != nil {
+									log.Errorf("edge-28: invalid input to  DigitalToGPIOValue")
+									continue
 								}
 								_, err = i.processWrite(pnt, wv, rest, counter, false)
-							case pointList.DO1, pointList.DO2, pointList.DO3, pointList.DO4, pointList.DO5:
-								_, err = i.processWrite(pnt, wv, rest, counter, false)
-							case pointList.UO1, pointList.UO2, pointList.UO3, pointList.UO4, pointList.UO5, pointList.UO6, pointList.UO7:
-								_, err = i.processWrite(pnt, wv, rest, counter, true)
 							}
-						} //READ TODO add in all the extra GPIO
-						_pnt.UUID = pnt.UUID
-						switch pnt.IoID {
-						case pointList.UI1:
-							pv = getUI.Val.UI1.Val
-							_, err = i.processRead(pnt, pv, counter)
-						case pointList.UI2:
-							pv = getUI.Val.UI2.Val
-							_, err = i.processRead(pnt, pv, counter)
-						case pointList.DI1:
-							pv = GPIOValueToDigital(getDI.Val.DI1.Val)
-							_, err = i.processRead(pnt, pv, counter)
+
+						case pointList.UO1, pointList.UO2, pointList.UO3, pointList.UO4, pointList.UO5, pointList.UO6, pointList.UO7:
+							//fmt.Println(*(pnt))
+							//fmt.Printf("%+v\n", *(pnt))
+							if pnt.Priority != nil {
+								wv, err = GetGPIOValueForUOByType(pnt)
+								if err != nil {
+									log.Error(err)
+									continue
+								}
+								_, err = i.processWrite(pnt, wv, rest, counter, true)
+								if err != nil {
+									log.Error(err)
+									continue
+								}
+							}
+
+						//INPUTS
+						case pointList.DI1, pointList.DI2, pointList.DI3, pointList.DI4, pointList.DI5, pointList.DI6, pointList.DI7:
+							readValStruct, readValType, err = utils.GetStructFieldByString(getDI.Val, pnt.IoID)
+							if err != nil {
+								log.Error(err)
+								continue
+							} else if readValType != "struct" {
+								log.Error("edge-28: IoID does not match any points from Edge28")
+								continue
+							}
+							rv = reflect.ValueOf(readValStruct).FieldByName("Val").Float()
+							rv, err = GetValueFromGPIOForUIByType(pnt, rv)
+							if err != nil {
+								log.Error(err)
+								continue
+							}
+							_, err = i.processRead(pnt, rv, counter)
+
+						case pointList.UI1, pointList.UI2, pointList.UI3, pointList.UI4, pointList.UI5, pointList.UI6, pointList.UI7:
+							readValStruct, readValType, err = utils.GetStructFieldByString(getUI.Val, pnt.IoID)
+							if err != nil {
+								log.Error(err)
+								continue
+							} else if readValType != "struct" {
+								log.Error("edge-28: IoID does not match any points from Edge28")
+								continue
+							}
+							rv = reflect.ValueOf(readValStruct).FieldByName("Val").Float()
+							rv, err = GetValueFromGPIOForUIByType(pnt, rv)
+							if err != nil {
+								log.Error(err)
+								continue
+							}
+							_, err = i.processRead(pnt, rv, counter)
 						}
 					}
 				}
@@ -168,11 +201,94 @@ func (i *Instance) polling(p polling) error {
 		} else {
 			return false, nil
 		}
-
 	}
 	err := poll.Poll(context.Background(), f)
 	if err != nil {
-		return nil
+		return err
 	}
 	return nil
+}
+
+// GetGPIOValueForUOByType converts the point value to the correct edge28 UO GPIO value based on the IoType
+func GetGPIOValueForUOByType(point *model.Point) (float64, error) {
+	var err error
+	var result float64
+	if !utils.ExistsInStrut(UOTypes, point.IoType) {
+		err = errors.New(fmt.Sprintf("edge-28: skipping %v, IoType %v not recognized.", point.IoID, point.IoType))
+		return 0, err
+	}
+	//fmt.Println("point")
+	//fmt.Printf("%+v\n", point)
+	fmt.Println("point.Priority")
+	fmt.Printf("%+v\n", point.Priority)
+	fmt.Println("point.Priority.P16")
+	fmt.Printf("%+v\n", point.Priority.P16)
+	//wv = *(point.PresentValue)   //TODO: use PresentValue instead of Priority 16 value
+	if numbers.Float64PointerIsNil(point.Priority.P16) {
+		return 0, errors.New("no value to write.")
+	} else {
+		result = *(point.Priority.P16)
+	}
+
+	switch point.IoType {
+	case UOTypes.DIGITAL:
+		result, err = edge28.DigitalToGPIOValue(result)
+	case UOTypes.PERCENT:
+		result = edge28.PercentToGPIOValue(result)
+	case UOTypes.VOLTSDC:
+		result = edge28.VoltageToGPIOValue(result)
+	default:
+		err = errors.New("UO IoType is not a recognized type")
+	}
+	if err != nil {
+		return 0, err
+	} else {
+		return result, nil
+	}
+}
+
+// GetValueFromGPIOForUIByType converts the GPIO value to the scaled UI value based on the IoType
+func GetValueFromGPIOForUIByType(point *model.Point, value float64) (float64, error) {
+	var err error
+	var result float64
+
+	if !utils.ExistsInStrut(UITypes, point.IoType) {
+		err = errors.New(fmt.Sprintf("edge-28: skipping %v, IoType %v not recognized.", point.IoID, point.IoType))
+		return 0, err
+	}
+	switch point.IoType {
+	case UITypes.RAW:
+		result = value
+	case UITypes.DIGITAL:
+		result = edge28.GPIOValueToDigital(value)
+	case UITypes.PERCENT:
+		result = edge28.GPIOValueToPercent(value)
+	case UITypes.VOLTSDC:
+		result = edge28.GPIOValueToVoltage(value)
+	case UITypes.MILLIAMPS:
+		result = edge28.ScaleGPIOValueTo420ma(value)
+	case UITypes.RESISTANCE:
+		result = edge28.ScaleGPIOValueToResistance(value)
+	case UITypes.THERMISTOR10KT2:
+		resistance := edge28.ScaleGPIOValueToResistance(value)
+		result, err = thermistor.ResistanceToTemperature(resistance, thermistor.T210K)
+	case UITypes.THERMISTOR10KT3:
+		resistance := edge28.ScaleGPIOValueToResistance(value)
+		result, err = thermistor.ResistanceToTemperature(resistance, thermistor.T310K)
+	case UITypes.THERMISTOR20KT1:
+		resistance := edge28.ScaleGPIOValueToResistance(value)
+		result, err = thermistor.ResistanceToTemperature(resistance, thermistor.T120K)
+	case UITypes.THERMISTORPT100:
+		resistance := edge28.ScaleGPIOValueToResistance(value)
+		result = resistance
+		//result, err = thermistor.ResistanceToTemperature(resistance, thermistor.PT100)
+	case UITypes.THERMISTORPT1000:
+		resistance := edge28.ScaleGPIOValueToResistance(value)
+		result = resistance
+		//result, err = thermistor.ResistanceToTemperature(resistance, thermistor.PT1000)
+	default:
+		err = errors.New("UI IoType is not a recognized type")
+		return 0, err
+	}
+	return result, nil
 }

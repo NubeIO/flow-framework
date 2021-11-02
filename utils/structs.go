6341d41ac67e8bd8421a38d30b153a54720d9579
Marc Dallen <marc@enviroservices.com.au>
2021-11-02 14:38:16 +1100

    WIP. issues with updating point values.

--- utils/structs.go
+++ utils/structs.go
@@ -49,5 +49,6 @@ func GetStructFieldByString(arr interface{}, toGet string) (interface{}, string,
 		err := errors.New("GetStrutFieldByString(): cannot find field in struct")
 		return nil, "", err
 	}
-	return f.Interface(), f.Type().String(), nil
+	//return f.Interface(), f.Type().String(), nil
+	return f.Interface(), f.Kind().String(), nil
 }

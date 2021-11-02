87c8feb89edf0054adf6424949255b2a9004646a
Marc Dallen <marc@enviroservices.com.au>
2021-11-02 14:49:20 +1100

    Merge remote-tracking branch 'origin/marc/edge28-plugin-scaling' into marc/edge28-plugin-scaling
    # Conflicts:
    #	plugin/nube/protocals/edge28/polling.go
    #	utils/structs.go

--- utils/structs.go
+++ utils/structs.go
@@ -51,4 +51,5 @@ func GetStructFieldByString(arr interface{}, toGet string) (interface{}, string,
 	}
 	//return f.Interface(), f.Type().String(), nil
 	return f.Interface(), f.Kind().String(), nil
+
 }
